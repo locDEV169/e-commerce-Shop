038b2be1b205b164c7eca9f80e48acdf0348c8d1
locDEV169 <loc16910@gmail.com>
2021-08-18 23:59:41 +0700

    MockProduct(learn React Card component)+fake data

---
+++ src/component/Products/Products.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Grid} from '@material-ui/core';
+
+import Product from './Product/Product';
+
+const products =[
+    {id:1, name:'Shoes', description:'running shoes',price:"$5",image:'https://i.pinimg.com/originals/3d/b9/3a/3db93a893f0bbae0a7194daed729e22e.jpg'},
+    {id:2,name:'Macbook', description:'Apple macbook',price:"$5",image:'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mbt-gt18-lead-1528903698.jpg?resize=980:*'}
+]
+
+const Products =() => {
+    console.log("s",products)
+    return(
+        <main>
+            <Grid container justifyContent="center" spacing={4}>
+                {products.map((product) => {
+                    return(
+                        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                            <Product product={product}/>
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </main>
+    )
+}
+export default Products;
